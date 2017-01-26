6c65d2bbc01b520a67ad2dd112c0f0ca8d960deb
Jussi <K8270@student.jamk.fi>
2017-01-26 14:53:13 +0200

    kiuas alkaa

--- CarApplication/Program.cs
+++ CarApplication/Program.cs
@@ -10,7 +10,26 @@ namespace CarApplication
     {
         static void Main(string[] args)
         {
+            // create a new instance from car class.
+            Car car = new Car();
+            car.Model = "Datsun 100A";
+            car.Color = "Nigerian Black";
+            car.Engine = 1.0;
+            car.Speed = 120;
+            car.FuzzyDice = true;
+            car.DoorCount = 2;
+            car.Accelerate();
+            car.PrintData();
+            Car.SomeProperty = 12;
+            car.Brake(50);
+            car.PrintData();
 
+            //create another car.
+            string model = "Speedster";
+            Car nascar = new Car(model);
+            nascar.PrintData();
+            nascar.Color = "MUSTA";
+            Console.WriteLine("Nascar color is: " + nascar.Color);
         }
     }
 }
