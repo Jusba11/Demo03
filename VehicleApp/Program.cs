784fd31b877f6d64dadf39712cc16a688112fd82
Jussi <K8270@student.jamk.fi>
2017-01-31 21:17:47 +0200

    Vehicle valmis

---
+++ VehicleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+
+            vehicle.Name = "Ferrari";
+            vehicle.Speed = 200;
+            vehicle.Tyres = 4;
+
+            string tiedot = vehicle.ToString(vehicle.Name, vehicle.Speed, vehicle.Tyres);
+            Console.WriteLine(tiedot);
+
+
+            vehicle.Name = "Volvo";
+            vehicle.Speed = 10;
+            vehicle.Tyres = 2;
+
+
+            tiedot = vehicle.ToString(vehicle.Name, vehicle.Speed, vehicle.Tyres);
+            Console.WriteLine(tiedot);
+
+
+        }
+    }
+}
---
+++ VehicleApp/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle name: " + Name);
+            Console.WriteLine("Vehicle speed: " + Speed);
+            Console.WriteLine("Number of tyres: " + Tyres);
+        }
+
+        public string ToString(string Name, int Speed, int Tyres)
+        {
+            return Name + " " + Speed + " " + Tyres;
+        }
+    }
+}
