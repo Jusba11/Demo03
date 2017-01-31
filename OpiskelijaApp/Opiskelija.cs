1395ebeb666dae7991823206e57f4e4f35c87964
Jussi <K8270@student.jamk.fi>
2017-01-31 22:07:06 +0200

    Opiskelija-homma.

---
+++ OpiskelijaApp/Opiskelija.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpiskelijaApp
+{
+    class Opiskelija
+    {
+        public int Ika { get; set; }
+        public double Keskiarvo { get; set; }
+        public string Harrastus { get; set; }
+
+        
+        public void NautiOlut()
+        {
+            Console.WriteLine("Nams.");
+        }
+
+        public void Nuku()
+        {
+            Console.WriteLine("Zzzz..");
+        }
+        
+    }
+}
---
+++ OpiskelijaApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpiskelijaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija Matti = new Opiskelija();
+            Matti.Harrastus = "Galj";
+            Matti.Ika = 23;
+            Matti.Keskiarvo = 1.2;
+
+            Opiskelija Seppo = new Opiskelija();
+            Seppo.Harrastus = "Golf";
+            Seppo.Ika = 30;
+            Seppo.Keskiarvo = 4.3;
+            
+            Opiskelija Jorma = new Opiskelija();
+            Jorma.Harrastus = "Kalastus";
+            Jorma.Ika = 12;
+            Jorma.Keskiarvo = 5;
+            
+            Opiskelija Irma = new Opiskelija();
+            Irma.Harrastus = "Juoksu";
+            Irma.Ika = 35;
+            Irma.Keskiarvo = 1;
+            
+            Opiskelija Kyllikki = new Opiskelija();
+            Kyllikki.Harrastus = "Kudonta";
+            Kyllikki.Ika = 98;
+            Kyllikki.Keskiarvo = 4.6;
+
+            string[] names = new string[5] { "Matti", "Seppo", "Jorma", "Irma", "Kyllikki" };
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+        }
+    }
+}
