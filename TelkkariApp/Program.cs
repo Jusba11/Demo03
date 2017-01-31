a80ee91af6d28058334812cabfbac9443722993b
Jussi <K8270@student.jamk.fi>
2017-01-31 19:46:38 +0200

    TV valmis

--- TelkkariApp/Program.cs
+++ TelkkariApp/Program.cs
@@ -10,6 +10,72 @@ namespace TelkkariApp
     {
         static void Main(string[] args)
         {
+            Tv tv = new Tv();
+            tv.Brightness = 300;
+            tv.Volume = 5;
+            tv.Channel = 1;
+            tv.Guide = false;
+            tv.isOn = false;
+
+            do
+            {
+                do
+                {
+                    Console.Write("Turn TV on by pressing Y on your keyboard >");
+                    string syote = Console.ReadLine();
+
+                    if (syote == "Y" || syote == "y")
+                    {
+                        tv.isOn = true;
+                    }
+
+                } while (!tv.isOn);
+
+                Console.Clear();
+
+                tv.ShowStatus();
+                tv.ShowMenu();
+
+                do
+                {
+                    Console.Write("What do you what to do? press a corresponding key >");
+                    int syote = int.Parse(Console.ReadLine());
+
+                    switch (syote)
+                    {
+                        case 1:
+                            Console.Write("Choose a channel: >");
+                            syote = int.Parse(Console.ReadLine());
+                            tv.ChangeChannel(syote); break;
+                        case 2:
+                            Console.Write("Choose new brightness level: >");
+                            syote = int.Parse(Console.ReadLine());
+                            tv.ChangeBrightness(syote); break; // brightness
+
+                        case 3:
+                            Console.Write("Choose new Volume level: >");
+                            syote = int.Parse(Console.ReadLine());
+                            tv.VolumeSet(syote); break;
+
+                        case 4:
+                            tv.ToggleGuide();
+                            break;  //guide
+
+                        case 0:
+                            tv.isOn = false;
+                            break; // turnOff
+                    }
+
+                    Console.Clear();
+                    tv.ShowStatus();
+                    tv.ShowMenu();
+
+                } while (tv.isOn);
+
+                Console.Clear();
+                Console.WriteLine("TV IS NOW OFF!");
+
+            } while (1 == 1);
         }
     }
 }
---
+++ TelkkariApp/tv.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelkkariApp
+{
+    class Tv
+    {
+        public int Channel { get; set; }
+        public int Volume { get; set; }
+        public int Brightness { get; set; }
+        public bool Guide { get; set; } // guide-tila päällä tai pois.
+        public bool isOn { get; set; }
+
+        public void PowerOn()
+        {
+            Console.WriteLine("TV IS NOW ON");
+        }
+
+        public void ToggleGuide()
+        {
+            if (!Guide)
+            {
+                Guide = true;
+            }
+            else
+            {
+                Guide = false;
+            }
+        }
+        public void VolumeSet(int a)
+        {
+            Volume = a;
+        }
+        public void ChangeChannel(int b)
+        {
+            Channel = b;
+        }
+        
+        public void ChangeBrightness(int c)
+        {
+            Brightness = c;
+        }
+
+
+        public void ShowStatus()
+        {
+            Console.WriteLine("TV IS NOW ON! \n");
+
+            Console.WriteLine("TV brightness: " + Brightness);
+
+            Console.WriteLine("TV Volume: " + Volume);
+
+            Console.WriteLine("Channel: " + Channel);
+
+            Console.WriteLine("is guide showing: " + Guide);
+        }
+        public void ShowMenu()
+        {
+
+            Console.WriteLine("\n\n\n1: Change the Channel");
+
+            Console.WriteLine("2. Adjust Brightness");
+
+            Console.WriteLine("3: Adjust Volume");
+
+            Console.WriteLine("4: Toggle Guide");
+
+            Console.WriteLine("0: Turn TV off.");
+        }
+
+    }
+}
